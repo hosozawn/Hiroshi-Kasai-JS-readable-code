ce34ab7a5f8949245ffcf9f5fdb0a3c067838e41
kasaih <hiroshi.kasai@justsystems.com>
2019-09-24 11:34:03 +0900

    spec1: 仕様1を実装。index.htmlの実行で、結果文字列はコンソール出力とする。

---
+++ code/PageCode.js
@@ -0,0 +1,15 @@
+class PageCode {
+
+  constructor() {
+  }
+
+  /**
+   * 仕様1実行
+   */
+  execSpec1 = function () {
+    window.console.log("上手");
+  }
+}
+
+page = new PageCode();
+page.execSpec1();
